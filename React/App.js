fb200b6c57d32a7ea82391d8d195813cf4bff99e
AkhilaThambi96 <87758120+AkhilaThambi96@users.noreply.github.com>
2021-07-29 12:44:46 +0530

    Add files via upload

---
+++ React/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+import App1 from './components/Exmp';
+import Toggle from './components/Exmp1';
+import Clock from './components/Exmp2';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     <App1 />
+     <Toggle />
+     <Clock />
+      </header>
+    </div>
+  );
+}
+
+export default App;
