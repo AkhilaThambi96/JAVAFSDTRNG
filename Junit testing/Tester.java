7603e62d9a5350076b763d8e4fd04e17355839e2
Akhila M Thambi <87758120+AkhilaThambi96@users.noreply.github.com>
2021-08-16 18:18:41 +0530

    Add files via upload

---
+++ Junit testing/Tester.java
@@ -0,0 +1,20 @@
+package com.ust.testing;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+	@RunWith(Suite.class)
+	@SuiteClasses({Junit.class,Junit1.class})
+
+public class Tester {
+		public static void main(String args[])
+		{
+			Result r = JUnitCore.runClasses(Tester.class);
+			System.out.println(r.wasSuccessful());
+			
+		}
+}
